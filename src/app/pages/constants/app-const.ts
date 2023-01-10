5004c16304a072c75bdaa838baf478227f70e02f
jsl_takumi_akiba <takumi_akiba@a.jsl.co.jp>
2023-01-11 08:51:20 +0900

    yes-no dialog

--- src/app/pages/constants/app-const.ts
+++ src/app/pages/constants/app-const.ts
@@ -1,4 +1,6 @@
 export class AppConst {
   // Session Storage keys
   static readonly STORAGE_KEY_USER = 'USER';  
+  static readonly YES_NO_DIALOG_HEIGHT = '175px';
+  static readonly YES_NO_DIALOG_WIDTH = '315px';
 }
